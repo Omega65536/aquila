e76125dc1ca43b948d6666775b392356e8be4616
Omega65536 <33597451+Omega65536@users.noreply.github.com>
2023-07-05 15:24:04 +0200

    Add tests; run with make check

---
+++ tests/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $(dirname $0)
+
+arr=("test_print" "test_math" "test_variables" "test_comparison")
+
+for name in "${arr[@]}"
+do
+    ../src/aquila "./$name.aq" > "$name.out"
+    diff -s "$name.ref" "$name.out"
+    rm -f "$name.out"
+done
